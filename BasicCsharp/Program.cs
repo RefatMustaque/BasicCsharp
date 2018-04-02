52501d522296197408cfe52df4ece134298497f0
Refat Bin Mustaque <r.b.m.tonmoy@gmail.com>
2018-04-02 22:18:44 +0600

    Namspace Example Included as a project.

--- BasicCsharp/Program.cs
+++ BasicCsharp/Program.cs
@@ -10,6 +10,8 @@ namespace BasicCsharp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
